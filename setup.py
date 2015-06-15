e660252ca6cc888f6f6394e1c680ec88cf72e126
Philip Willett <philip.d.willett@gmail.com>
2015-06-14 23:16:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ README = path.abspath(path.join(path.dirname(__file__), 'README.rst'))
 setup(
       name='python-redis-log',
       version='0.1.2',
-      description='Redis pub/sub logging handler for python',
+      description='Redis pub/sub/list logging handler for python',
       long_description=open(README).read(),
       author='Jed Parsons',
       author_email='jed@jedparsons.com',
-      url='https://github.com/jedp/python-redis-log',
+      url='https://github.com/phil-willett/python-redis-log',
       packages=['redislog'],
       license='MIT',
       install_requires=['redis', 'simplejson']
