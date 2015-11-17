5264ad12369bf686349dca681c8b58e3747e5227
Philip Willett <philip.d.willett@gmail.com>
2015-11-17 06:00:30 -0600

    Update handlers.py
    Removed duration from handlers.py

--- redislog/handlers.py
+++ redislog/handlers.py
@@ -64,8 +64,8 @@ class RedisListHandler(logging.Handler):
     """
 
     @classmethod
-    def to(cklass, key, max_messages=None, host=None, port=None, db=None, duration=None, level=logging.NOTSET):
-        return cklass(key, max_messages, redis.Redis(host=host or 'localhost', port=port or 6379, db=db or 0), duration=duration or 0, level=level)
+    def to(cklass, key, max_messages=None, host=None, port=None, db=None, level=logging.NOTSET):
+        return cklass(key, max_messages, redis.Redis(host=host or 'localhost', port=port or 6379, db=db or 0), level=level)
 
     def __init__(self, key, max_messages, redis_client, duration, level=logging.NOTSET):
         """
@@ -76,7 +76,6 @@ class RedisListHandler(logging.Handler):
         self.redis_client = redis_client
         self.formatter = RedisFormatter()
         self.max_messages = max_messages
-        self.duration = duration
 
     def emit(self, record):
         """
