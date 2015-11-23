2aa0c09859848da3067b15138048d5f6cb220389
Philip Willett <philip.d.willett@gmail.com>
2015-11-23 11:41:04 -0600

    Update handlers.py

--- redislog/handlers.py
+++ redislog/handlers.py
@@ -22,7 +22,7 @@ class RedisFormatter(logging.Formatter):
         return json.dumps(data, skipkeys=False, ensure_ascii=True, 
            check_circular=True, allow_nan=True, cls=None, 
            indent=None, separators=None, encoding='utf-8', 
-           default=None)
+           default=str)
 
 
 class RedisHandler(logging.Handler):
