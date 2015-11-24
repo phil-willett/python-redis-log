56d471e7aac7aa28643acdd1f186c6d9f85e6fc7
Philip Willett <philip.d.willett@gmail.com>
2015-11-23 21:38:31 -0600

    Update logger.py

--- redislog/logger.py
+++ redislog/logger.py
@@ -69,7 +69,7 @@ class RedisLogRecord(logging.LogRecord):
             "level": levelAsString(lvl),
             "filename": fn,
             "line_no": self.lineno,
-            "msg": list(msg),
+            "msg": str(msg),
             "args": list(args),
             "time": datetime.datetime.utcnow(),
             "username": self.username,
